b63e61c0e8e04a5bd488768fbdb1aa361a54cc7e
lakshmi <lmethukumalli@gmail.com>
2014-10-09 22:46:13 -0500

    index file

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ var http = require('http');
 var fs = require('fs');
 
 fs.readFile('homepage.php', function (err, html) {
-    if (err) {
+    /*if (err) {
         throw err;
-    }
+    }*/
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
